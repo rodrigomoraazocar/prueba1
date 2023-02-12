55eccdb81cc17494e9ce00177265260778ac7693
Rodrigo A. Mora Azócar <brazuk25@gmail.com>
2023-02-11 21:38:51 -0300

    Mapeando Tabla Empleado

---
+++ src/main/java/br/com/rorolabs/prueba1/model/Empleado.java
@@ -0,0 +1,65 @@
+package br.com.rorolabs.prueba1.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("EMPLEADO")
+public class Empleado {
+
+	@Id
+	@Column("idEmpleado")
+	private int idEmpleado;
+	
+	@Column("nombre")
+	private String nombre;
+
+	@Column("direccion")
+	private String direccion;
+
+	@Column("email")
+	private String email;
+
+	public Empleado() {
+	}
+
+	public Empleado(int idEmpleado, String nombre, String direccion, String email) {
+		this.idEmpleado = idEmpleado;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.email = email;
+	}
+
+	public int getIdEmpleado() {
+		return idEmpleado;
+	}
+
+	public void setIdEmpleado(int idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
